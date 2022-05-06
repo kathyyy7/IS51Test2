dc32dd63de027c5aebc74d37192a86a92480ce9a
Kathy Franco <kfrancoba@mail.fresnostate.edu>
2022-05-05 22:36:42 -0700

    pseudo-code

--- test_2.py
+++ test_2.py
@@ -4,10 +4,31 @@ capture user's input 24 times for their grades
 each time we capture the user's input, we apprend te number to the list 
 
 sort the list ascending, then splice the items starting at the index 2
-We sum them up and divide by 3
+We sum them up and divide by the total of grades 
 
 output a message to the user
 
 end
+
+""""""""
+
+PSEUDO CODE
+create list
+capture input 
+append number to list
+
+capture input 
+append number to list
+
+capture input 
+append number to list
+
+capture input 
+append number to list
+
+capture input 
+append number to list
+
+print message to user
 
 """""
