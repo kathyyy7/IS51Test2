5b0a7d3dfbfb86b21ca95f244f4cdf09906d1091
Kathy Franco <kfrancoba@mail.fresnostate.edu>
2022-05-05 22:03:35 -0700

    structured english

---
+++ test_2.py
@@ -0,0 +1,13 @@
+"""""
+create a list to store 24 numbers (float)
+capture user's input 24 times for their grades
+each time we capture the user's input, we apprend te number to the list 
+
+sort the list ascending, then splice the items starting at the index 2
+We sum them up and divide by 3
+
+output a message to the user
+
+end
+
+"""""
